49274d26bfb1d0e6c227e0081e051d262e66b5b1
BraveLilToaster <penguindude2691@gmail.com>
2018-03-14 23:37:07 -0400

    Add inner shadow at bottom of Hero.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Hero = (props) => {
-const HeroBackground = styled.div`
-	position: relative;
-  overflow: hidden;
-  min-height: 100vh;
-  @media (min-width: 768px) {
-    min-height: 85vh;
-  }
-`
+  const HeroBackground = styled.div`
+    position: relative;
+    overflow: hidden;
+    min-height: 100vh;
+    @media (min-width: 768px) {
+      min-height: 85vh;
+    }
+  `
+
+  const BelowTheFoldArrow = styled.div`
+  `
+  const HeroBottomGradient = styled.div`
+    position: absolute;
+    height: 10vh;
+    width: 100%;
+    bottom: 0px;
+    z-index: 6;
+    background: linear-gradient(180deg,rgba(0, 0, 0, 0) 0%,rgba(0, 0, 0, 0.33) 67%,rgba(0, 0, 0, 0.54) 100%);
+  `
   return(
   <HeroBackground>
     {props.children}
+    <HeroBottomGradient />
   </HeroBackground>
   )
 
