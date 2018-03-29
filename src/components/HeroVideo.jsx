fcbea3621f5d301553cbb62619870d27dccf32be
BraveLilToaster <penguindude2691@gmail.com>
2018-03-28 23:03:08 -0400

    Add video overlay to make HeroText more legible.

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -3,24 +3,37 @@ import styled from 'styled-components'
 
 const HeroVideo = (props) => {
   const HeroVideo = styled.video`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  min-height: 100vh;
-  @media (min-width: 768px) {
-    min-width: 1280px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     min-height: 100vh;
-  }
-  @media (min-width: 1500px) {
+    @media (min-width: 768px) {
+      min-width: 1280px;
+      min-height: 100vh;
+    }
+    @media (min-width: 1500px) {
+      width: 100%;
+    }
+  `
+  const VideoOverlay = styled.div`
+    height: 100%;
     width: 100%;
-  }
-`
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    z-index: 2;
+    background: #000;
+    opacity: 0.5;
+  `
   return(
-    <HeroVideo loop muted autoPlay poster="http://res.cloudinary.com/bravetoaster/image/upload/v1520916557/IMG_8630.jpg">
-      <source src="https://res.cloudinary.com/bravetoaster/video/upload/v1521134860/The%20Dropout/DropoutReel720.webm" type="video/webm"></source>
-      <source src="https://res.cloudinary.com/bravetoaster/video/upload/v1521134774/The%20Dropout/DropoutReel720mp4.mp4" type="video/mp4"></source>
-    </HeroVideo>
+    <div>
+      <VideoOverlay />
+      <HeroVideo loop muted autoPlay poster="http://res.cloudinary.com/bravetoaster/image/upload/v1520916557/IMG_8630.jpg">
+        <source src="https://res.cloudinary.com/bravetoaster/video/upload/v1521134860/The%20Dropout/DropoutReel720.webm" type="video/webm"></source>
+        <source src="https://res.cloudinary.com/bravetoaster/video/upload/v1521134774/The%20Dropout/DropoutReel720mp4.mp4" type="video/mp4"></source>
+      </HeroVideo>
+    </div>
   )
 
 }
